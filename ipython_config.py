77ae8874d1486e3a910f71d287af24b1eba1e753
Glenn Ehrlich <glenn.ehrlich@kinetx.com>
2019-08-14 23:17:21 -0700

    Added ipython config files.

---
+++ ipython_config.py
@@ -0,0 +1,4 @@
+# Symlink this file to ~/.ipython/profile_default/ipython_config.py
+
+c.InteractiveShellApp.extensions = ['autoreload']
+c.InteractiveShellApp.exec_lines = ['%autoreload 2']
---
+++ ipython_kernel_config.py
@@ -0,0 +1,2 @@
+# Symlink this file to ~/.ipython/profile_default/ipython_kernel_config.py
+
